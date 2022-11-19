22d323b979827a50becc3e475919514052b20ad4
KylePavone <scaryfase1237373@gmail.com>
2022-11-19 05:32:04 +0300

    userprofile

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -10,6 +10,7 @@ window.onload = function() {
     activeTown.addEventListener("click", function(){
         activeTown.classList.toggle("_active")
     })
+    let headerElement = document.querySelector(".header")
     let bodyElement = document.querySelector("body")
     let navMenu = document.querySelector(".header__nav-menu")
     let layer = document.querySelector(".layer")
@@ -18,16 +19,27 @@ window.onload = function() {
         navMenu.classList.add("_active")
         layer.classList.add("__active")
         bodyElement.classList.add("_active")
+        headerElement.classList.add("_active")
        
     })
 
     let closeButton = document.querySelector(".__cross")
     closeButton.addEventListener("click", function() {
-        layer.classList.remove("__active")
+        layer.classList.remove("__active")  
         navMenu.classList.remove("_active")
         bodyElement.classList.remove("_active")
+        headerElement.classList.remove("_active")
     })
 
-    let navButton = document.querySelector("")
 
+   $(".table-bonuses__content").click(function(){
+        if ($(".table-bonuses").hasClass("one")) {
+            $(".table-bonuses__content").not($(this)).removeClass("_active")
+            $(".table-bonuses__content").not($(this)).children(".table-bonuses__arrow").removeClass("_active")
+            $(".table-bonuses__spoller").not($(this).next()).slideUp(0)
+            
+        }
+        $(this).toggleClass("_active").next().slideToggle(0)
+        $(this).children(".table-bonuses__arrow").toggleClass("_active")
+    })
 }
